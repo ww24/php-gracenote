c09f70b9dcb11cfdfc2c90c4776ffd4ad4017687
Takenori Nakagawa <ww24gm@gmail.com>
2015-09-03 01:55:15 +0900

    Add app example

---
+++ example/app/www/js/controllers.js
@@ -0,0 +1,156 @@
+/**
+ * Controllers
+ *
+ */
+
+angular.module("starter.controllers", [])
+.controller("PlayerCtrl", function($scope, $http, $ionicLoading, $ionicModal, Playlist, Settings) {
+  $scope.Playlist = Playlist;
+  console.log(Playlist);
+
+  $scope.track = null;
+
+  $ionicModal.fromTemplateUrl("templates/modal-player.html", {
+    scope: $scope,
+    animation: "slide-in-up"
+  }).then(function (modal) {
+    console.log(modal);
+    $scope.modal = modal;
+  });
+
+  $scope.$watch("track.audio.paused", function (new_val) {
+    if ($scope.track === null) {
+      return;
+    }
+    if (new_val) {
+      $scope.track.icon = "ion-play";
+    } else {
+      $scope.track.icon = "ion-pause";
+    }
+  });
+
+  // Gracenote feedback event
+  $scope.feedback = function () {
+    $http.get(Settings.endpoint + "/");
+  };
+
+  // play music
+  $scope.play = function () {
+    var audio = $scope.track.audio;
+
+    console.log(a = audio);
+
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  };
+
+  // stop music when modal hidden
+  $scope.$on("modal.hidden", function () {
+    var audio = $scope.track.audio;
+    if (audio === null) {
+      return;
+    }
+    $scope.track.audio.pause();
+  });
+
+  $scope.seek = function () {
+    console.log("seek");
+    var audio = $scope.track.audio;
+    audio.currentTime = $scope.track.time / 1000;
+  };
+
+  // select a track
+  $scope.select = function () {
+    var song = this.song;
+
+    $ionicLoading.show({
+      template: "Loading..."
+    });
+
+    $http.get(Settings.endpoint + "/itunes", {
+      params: {
+        term: song.TRACK[0].TITLE[0].VALUE.trim(),
+        artist: ((song.TRACK[0].ARTIST || song.ARTIST)[0].VALUE).split(", ")[0].trim()
+      }
+    }).then(function (res) {
+      console.log(res.data);
+      var data = res.data;
+
+      $ionicLoading.hide();
+      $scope.modal.show();
+
+      var track = data.correct_result || data.results[0];
+      if (! track) {
+        console.log(data);
+        return console.error("track not found");
+      }
+
+      console.log(track.artistName, track.trackName);
+
+      console.log(track.previewUrl);
+
+      $scope.track = {
+        url: track.previewUrl,
+        artist: ((song.TRACK[0].ARTIST || song.ARTIST)[0].VALUE).split(", ")[0],
+        title: song.TRACK[0].TITLE[0].VALUE,
+        art: track.artworkUrl100,
+        // play or pause icon
+        icon: "ion-play",
+        audio: null,
+        time: 0,
+        time_end: 0
+      };
+
+      var audio = new Audio($scope.track.url);
+      audio.autoplay = false;
+      audio.preload = "auto";
+      audio.load();
+      audio.addEventListener("progress", function () {
+        console.log("progress");
+        $scope.track.time_end = Math.floor(audio.seekable.end(0) * 1000);
+      });
+
+      audio.addEventListener("timeupdate", function () {
+        $scope.$apply(function () {
+          $scope.track.time = Math.floor(audio.currentTime * 1000);
+        });
+      });
+
+      $scope.track.audio = audio;
+    });
+  };
+})
+.controller("SettingsCtrl", function($scope, $http, $timeout, $ionicLoading, Settings, Playlist) {
+  $http.get(Settings.endpoint).then(function (res) {
+    console.log("UserID:", res.data.user_id);
+
+    return $http.get(Settings.endpoint + "/fieldvalues");
+  }).then(function (res) {
+    console.log(res.data);
+
+    $scope.genres = [{ID:"", VALUE:"未選択"}].concat(res.data.genre);
+    $scope.moods = [{ID:"", VALUE:"未選択"}].concat(res.data.mood);
+    $scope.eras = [{ID:"", VALUE:"未選択"}].concat(res.data.era);
+  });
+
+  $scope.Settings = Settings;
+
+  $scope.save = function () {
+    if (Settings.endpoint.slice(-1) === "/") {
+      Settings.endpoint = Settings.endpoint.slice(0, -1);
+    }
+
+    Settings.save();
+    Playlist.create();
+
+    $ionicLoading.show({
+      template: "Saved"
+    });
+    $timeout(function () {
+      $ionicLoading.hide();
+    }, 500);
+  };
+});
---
+++ example/app/www/js/services.js
@@ -0,0 +1,80 @@
+/**
+ * Service
+ *
+ */
+
+angular.module("starter.services", [])
+.filter("time_mmss", function () {
+  return function (time) {
+    return moment(Number(time)).format("mm:ss");
+  };
+})
+.factory("Settings", function () {
+  var prefix = "Settings:";
+
+  var data = {
+    endpoint: localStorage.getItem("Settings:endpoint") || "http://localhost:8080",
+    artist: null,
+    title: null,
+    genre: null,
+    mood: null,
+    era: null,
+    save: function () {
+      var data = this;
+      var keys = Object.keys(data).map(function (key) {
+        if (typeof data[key] === "function") {
+          return false;
+        }
+
+        localStorage.setItem(prefix + key, data[key]);
+
+        return key;
+      }).filter(function (value) {return value});
+
+      return keys;
+    }
+  };
+
+  Object.keys(data).forEach(function (key) {
+    if (typeof data[key] === "function" || data[key] !== null) {
+      return false;
+    }
+
+    data[key] = localStorage.getItem(prefix + key) || "";
+  });
+
+  return data;
+})
+.factory("Playlist", function (Settings, $http) {
+  var data = {};
+
+  data.create = function () {
+    if (Settings.endpoint.length > 0 && (Settings.genre.length > 0 || Settings.mood.length > 0 || Settings.era.length > 0 || Settings.artist.length > 0)) {
+      if (Settings.title.length > 0 && Settings.artist.length === 0) {
+        return false;
+      }
+
+      $http.get(Settings.endpoint + "/create", {
+        params: {
+          artist_name: Settings.artist,
+          track_title: Settings.title,
+          genre: Settings.genre,
+          mood: Settings.mood,
+          era: Settings.era,
+          return_count: 5
+        }
+      }).then(function (res) {
+        data.radio_id = res.data.radio_id;
+        data.songs = res.data.album;
+      });
+
+      return true;
+    }
+
+    return false;
+  };
+
+  console.log("Create playlist:", data.create());
+
+  return data;
+});
